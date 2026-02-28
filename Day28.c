4bf09f9f98a44370d9dec350586dc67aa4bdc196
Arnab Roy <arnab8238@gmail.com>
2026-02-28 22:29:34 +0530

    Add files via upload

---
+++ Day28.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->next = *head; 
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != *head) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = *head;
+}
+
+void traverse(struct Node* head) {
+    if (head == NULL) return;
+    struct Node* temp = head;
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    struct Node* head = NULL;
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+        insertEnd(&head, val);
+    }
+    traverse(head);
+    return 0;
+}
